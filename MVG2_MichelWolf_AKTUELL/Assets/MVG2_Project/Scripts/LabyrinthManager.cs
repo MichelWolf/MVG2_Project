8282a3487f21efa6525f5280b7b1b057d9a69a79
MVG <michel.wolf@unitybox.de>
2019-12-03 14:45:08 +0100

    Update LabyrinthManager.cs

--- MVG2_MichelWolf_AKTUELL/Assets/MVG2_Project/Scripts/LabyrinthManager.cs
+++ MVG2_MichelWolf_AKTUELL/Assets/MVG2_Project/Scripts/LabyrinthManager.cs
@@ -89,6 +89,6 @@ public class LabyrinthManager : MonoBehaviourPunCallbacks, IPunObservable
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        throw new System.NotImplementedException();
+
     }
 }

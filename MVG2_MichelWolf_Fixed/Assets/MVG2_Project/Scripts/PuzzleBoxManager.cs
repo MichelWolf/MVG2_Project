27d02235f9eef5115c5c9cc1bbb439f0756cd085
MVG <michel.wolf@unitybox.de>
2019-12-17 13:30:15 +0100

    Network Instatiating Controller Prefabs

--- MVG2_MichelWolf_Fixed/Assets/MVG2_Project/Scripts/PuzzleBoxManager.cs
+++ MVG2_MichelWolf_Fixed/Assets/MVG2_Project/Scripts/PuzzleBoxManager.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class PuzzleBoxManager : MonoBehaviour {
+public class PuzzleBoxManager : MonoBehaviourPunCallbacks, IPunObservable  {
 
     [Header("Simon Says")]
     public bool SimonSuccess;
@@ -17,6 +19,17 @@ public class PuzzleBoxManager : MonoBehaviour {
     public GameObject hiddenLabyrinthMulti;
     public GameObject lockedLabyrinth;
     // Use this for initialization
+
+    [Header("Controller Setup")]
+    public GameObject ctrlLeftParent;
+    public GameObject ctrlRightParent;
+
+    public GameObject LeftPrefab;
+    public GameObject RightPrefab;
+
+    private GameObject l;
+    private GameObject r;
+
     void Start () 
     {
         lockedSimon.SetActive(true);
@@ -24,6 +37,14 @@ public class PuzzleBoxManager : MonoBehaviour {
 
         hiddenSimonMulti.SetActive(false);
         hiddenLabyrinthMulti.SetActive(false);
+        
+        if(PhotonNetwork.IsConnected)
+        {
+            l = PhotonNetwork.Instantiate("ContrModelLeft", ctrlLeftParent.transform.position, ctrlLeftParent.transform.rotation);
+            l.transform.SetParent(ctrlLeftParent.transform);
+            r = PhotonNetwork.Instantiate("ContrModelRight", ctrlRightParent.transform.position, ctrlRightParent.transform.rotation);
+            r.transform.SetParent(ctrlRightParent.transform);
+        }
     }
 	
 	// Update is called once per frame
@@ -64,5 +85,10 @@ public class PuzzleBoxManager : MonoBehaviour {
     public void FinishPuzzleBox()
     {
         Debug.Log("Puzzlebox vollständig gelöst!");
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        throw new System.NotImplementedException();
     }
 }

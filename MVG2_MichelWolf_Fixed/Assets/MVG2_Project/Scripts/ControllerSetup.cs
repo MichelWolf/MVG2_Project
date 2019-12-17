b23bf12e088f1aa52d3c4867506d42cc70df5fc8
MVG <michel.wolf@unitybox.de>
2019-12-17 14:01:39 +0100

    ADDED Controller disabling Collider if not controlled locally

---
+++ MVG2_MichelWolf_Fixed/Assets/MVG2_Project/Scripts/ControllerSetup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class ControllerSetup : MonoBehaviourPunCallbacks, IPunObservable
+{
+    // Use this for initialization
+    void Start () {
+		if (PhotonNetwork.IsConnected)
+        {
+            if(!photonView.IsMine)
+            {
+                GetComponentInChildren<SphereCollider>().enabled = false;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
